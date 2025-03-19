c59bade108f8d1db18781f4c03e2673d8c41257c
Prijith T <prijitht4@gmail.com>
2025-03-19 22:09:40 +0530

    update admin/regions

--- src/app/admin/manage-regions/page.tsx
+++ src/app/admin/manage-regions/page.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import AddRegions from './_components/add-regions'
 import RegionsTable from './_components/regions-table'
+import RegionsProvider from './_components/regions-provider'
 import { apiRequest } from '@/lib/api'
-
-import {
-    dehydrate,
-    HydrationBoundary,
-    QueryClient,
-} from '@tanstack/react-query'
 
 export const dynamic = 'force-dynamic'
 
@@ -16,26 +11,18 @@ export const fetchRegions = async () => {
 }
 
 async function ManageRegions() {
-
-    const queryClient=new QueryClient()
-
-  await queryClient.prefetchQuery({
-        queryKey:['regions'],
-        queryFn:fetchRegions
-    })
-
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className='px-4 py-4'>
-            <h1 className='admin-heading'>Manage Regions</h1>
-            <div className='flex justify-end pt-3 pr-4'>
-                <AddRegions />
+        <RegionsProvider>
+            <div className='px-4 py-4'>
+                <h1 className='admin-heading'>Manage Regions</h1>
+                <div className='flex justify-end pt-3 pr-4'>
+                    <AddRegions />
+                </div>
+                <div className='mt-2 mb-3 md:mb-28'>
+                    <RegionsTable />
+                </div>
             </div>
-            <div className='mt-2 mb-3 md:mb-28'>
-                <RegionsTable/>
-            </div>
-        </div>
-        </HydrationBoundary>
+        </RegionsProvider>
     )
 }
 
