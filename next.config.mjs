d72f2b0b09b194c1a9b1f874afc5a8b143502ae3
Prijith T <prijitht4@gmail.com>
2025-03-20 00:41:57 +0530

    fix deployment error5

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "yvihkbfh9232cvoj.public.blob.vercel-storage.com",
+        },
+      ],
       domains: ["yvihkbfh9232cvoj.public.blob.vercel-storage.com"], 
     },
   };
